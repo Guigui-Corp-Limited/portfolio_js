9569b45e3acd966205053f8b4bf0c064ff6a6445
Guigui0025 <guiguidr25@gmail.com>
2023-10-10 09:51:47 +0200

    Ajout card et projets

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,70 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
   <div class="jumbotron">
     <div class="container">
       <h1 class="display-4">Bienvenue sur Mon Site Web</h1>
-      <p class="lead">
-        Votre source fiable d'informations et de ressources en ligne.
-      </p>
-      <RouterLink to="/about" class="btn btn-primary btn-lg"
-        >En savoir plus sur nous</RouterLink
-      >
+    </div>
+  </div>
+
+  <!-- Conteneur flexbox pour les cartes -->
+  <div class="card-container">
+    <div class="card" style="width: 18rem">
+      <img src="public\images\couleur.png" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem">
+      <img
+        src="public\images\Plan de travail 1 copie 5.png"
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere else</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem">
+      <img
+        src="public\images\logo_indesign.png"
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+/* Styles pour le conteneur flexbox */
+.card-container {
+  display: flex; /* Utiliser la flexbox */
+  justify-content: space-evenly; /* Aligner les éléments avec un espace égal entre eux */
+}
+
+h1 {
+  text-align: center;
+}
+</style>
